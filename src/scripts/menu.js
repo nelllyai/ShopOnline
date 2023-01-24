a9f59fe14ab97b96d9a31673cf904329b0590fe8
nelllyai <worldpiece000@gmail.com>
2023-01-24 15:21:37 +0300

    add menu

---
+++ src/scripts/menu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const menuBtn = document.querySelector('.header__menu-button');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('header__menu-button_close');
+  menu.classList.toggle('menu_active');
+});
